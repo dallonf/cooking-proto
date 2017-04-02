66bfcda1360fd9782c190ea9b0cae8c4fac94787
DallonF <dallon.feldner@gmail.com>
2017-04-02 15:30:41 -0500

    Can now cook meals from the UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
+import { Meal } from '../types';
+import cookMeal from '../cooking/cooking';
 import { ingredientListForKeyList, default as allIngredients, IngredientInList } from '../cooking/all-ingredients';
 import './App.css';
 
 interface AppState {
   ingredients: string[];
   holding: string[];
+  cookedMeal: Meal | null;
 }
 
 class App extends React.Component<null, AppState> {
   state: AppState = {
     ingredients: [...allIngredients.map(i => i.key)],
     holding: [],
+    cookedMeal: null,
   };
 
   handleIngredientClick = (ingredient: IngredientInList) => (e: React.MouseEvent<HTMLAnchorElement>) => {
@@ -20,18 +24,56 @@ class App extends React.Component<null, AppState> {
     }));
   }
 
+  stopHolding = () => {
+    this.setState({ holding: [] });
+  }
+
+  cookMeal = () => {
+    this.setState((state: AppState) => {
+      const cookedMeal = cookMeal(ingredientListForKeyList(state.holding));
+      return {
+        cookedMeal,
+        holding: [],
+      };
+    });
+  }
+
+  confirmMeal = () => this.setState(state => ({ cookedMeal: null }));
+
   render() {
-    const { holding } = this.state;
+    const { holding, cookedMeal } = this.state;
+
+    if ( cookedMeal ) {
+      return (
+        <div>
+          <h2>{cookedMeal.name}</h2>
+          <ul>
+            <li>Heals {cookedMeal.hearts} hearts</li>
+          </ul>
+          <p>
+            {cookedMeal.description}
+          </p>
+          <button onClick={this.confirmMeal}>OK</button>
+        </div>
+      );
+    }
+
     const ingredients = ingredientListForKeyList(this.state.ingredients);
+    const canHoldMore = holding.length < 5;
     return (
       <div>
         <h2>Ingredients</h2>
         <ul>
           {ingredients.map(i => (
             <li key={i.key}>
-              <a href="#" onClick={this.handleIngredientClick(i)}>
-                {i.name}
-              </a>
+              {canHoldMore
+                ? (
+                  <a href="#" onClick={this.handleIngredientClick(i)}>
+                    {i.name}
+                  </a>
+                )
+                : i.name
+              }
             </li>
           ))}
         </ul>
@@ -41,10 +83,12 @@ class App extends React.Component<null, AppState> {
             <ul>
               {ingredientListForKeyList(holding).map((i, n) => (
                 <li key={n}>
-                    {i.name}
+                  {i.name}
                 </li>
               ))}
-            </ul> 
+            </ul>
+            <button onClick={this.stopHolding}>Stop Holding</button>
+            <button onClick={this.cookMeal}>Cook</button>
           </div>
         )}
       </div>
